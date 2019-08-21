afc995f8f2c4fff85f5a04a1b178a8d3955ef0db
Kim Joon <jk1094@naver.com>
2019-08-21 17:12:38 +0900

    Simple Example

--- Hello Express.js
+++ Hello Express.js
@@ -1,10 +1,13 @@
 var express = require('express')
 var app = express()
+var router = require('./router/main')(app)
+
+app.set('views' , __dirname +'/views')
+app.set('view engine', 'ejs')
+app.engine('html',require('ejs').renderFile)
+
 var server = app.listen(3000,function(){
     console.log('Example app listening on port 3000')
 })
-
-app.get('/',function(req, res) {
-    res.send('Hello Express!(GET)')    
-})
+app.use(express.static('public'));
 
